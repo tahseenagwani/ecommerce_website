6ba7e9da5a7a8cffe8827ae6a71706fee8c39afa
tahseenagwani <tahseenagwani786@gmail.com>
2023-05-23 23:59:01 +0530

    hjnj

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getProductById } from '../fetcher';
 
 const ProductDetail = () => {
-  return (
-    <div>ProductDetail</div>
+  const [product,setProduct]=useState({errorMessage:"",data:[]})
+
+  const params=useParams();
+  
+  useEffect(()=>{
+const fetcher=async()=>{
+ const responseObject=await getProductById(params.productId)
+setProduct(responseObject)
+}
+fetcher();
+  },[params.productId])
+  
+  
+  console.log(params.productId)
+  return ( 
+    
+    <div>product Details:{product.data.title}</div>
   )
 }
 
--- src/fetcher.js
+++ src/fetcher.js
@@ -27,3 +27,7 @@ export const getCategories = () => {
 export const getProducts = (id) => {
     return fetcher("/products?catId=" + id)
 }
+
+export const getProductById=(id)=>{
+    return fetcher("/products/" + id)
+}
